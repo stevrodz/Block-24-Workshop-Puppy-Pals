5885aa04fc992f21e7fd433be812e392512a24ee
stevrodz <rodriguezsl82@yahoo.com>
2024-10-22 13:07:28 -0500

    Sel a Feat Puppy

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,34 @@ import { puppyList } from "./data.js";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
   console.log(puppies);
+  const featuredPuppy = puppies.find((puppy) => puppy.id === featPupId);
   return (
     <>
       <div>
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
-        })} 
+          return (
+            <p
+              onClick={() => {
+                console.log("puppy id:", puppy.id);
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
+        {featPupId && (
+          <div>
+            <h2>{featuredPuppy.name}</h2>
+            <ul>
+              <li>Age: {featuredPuppy.age}</li>
+              <li>Email: {featuredPuppy.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
